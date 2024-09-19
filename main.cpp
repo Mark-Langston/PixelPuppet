5d64419dd1809cd444e3e1d03f54d1d34bdde1b7
Mark Langston <160653921+Mark-Langston@users.noreply.github.com>
2024-09-18 20:50:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    // Set application icon from file path
-    app.setWindowIcon(QIcon("C:/Users/mark_/OneDrive/Desktop/PixelPuppet/PixelPuppet/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/icon.png"));
+    // Set application icon using a relative path
+    app.setWindowIcon(QIcon("resources/icon.png"));
 
     PixelPuppetMain window;
     window.show();
